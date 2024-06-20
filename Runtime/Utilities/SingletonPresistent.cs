62603e9c27e8e07fc6fde437260601dbe23edef8
ManikandanDennis <manikandan.mp@zohocorp.com>
2024-06-20 17:16:19 +0530

    Name Change for a Class

--- Runtime/Utilities/SingletonPresistent.cs
+++ Runtime/Utilities/SingletonPresistent.cs
@@ -3,14 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 /// <summary>
-/// base class of singleton Presistent
+/// base class of singleton Persistent
 /// This class, don't destroy on load
 /// </summary>
 
 namespace Jambav.Utilities
 {
 
-    public class SingletonPresistent<T> : MonoBehaviour where T : MonoBehaviour
+    public class SingletonPersistent<T> : MonoBehaviour where T : MonoBehaviour
     {
         public static T sharedInstance { get; private set; }
         protected virtual void Awake()
