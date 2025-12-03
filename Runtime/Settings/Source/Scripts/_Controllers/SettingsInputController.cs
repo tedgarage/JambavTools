bf64874757cd3cdb1740dc8c1cef65793219bbcc
ManikandanDennis <manikandan.mp@zohocorp.com>
2025-12-03 14:24:40 +0530

    Add new UI elements and refactor SettingsInputController
    Added new GameObject and UI elements to SettingsCanvas.prefab for controller instructions. Refactored SettingsInputController.cs for improved formatting, readability, and consistency in long press and simultaneous button press handling logic.

--- Runtime/Settings/Source/Scripts/_Controllers/SettingsInputController.cs
+++ Runtime/Settings/Source/Scripts/_Controllers/SettingsInputController.cs
@@ -8,71 +8,71 @@ using Jambav.Utilities;
 namespace Jambav.Settings
 {
     public class SettingsInputController : Singleton<SettingsInputController>
-{
+    {
 
 
-    public Action OnSettingToggled;
+        public Action OnSettingToggled;
 
-    [Header("Restart Game")]
-    
-    private int currentPressedButtonIndex = -1;
+        [Header("Restart Game")]
 
-    // Game restart button Sequence
-    private List<OVRInput.RawButton> buttonOrderVRSeqList = new List<OVRInput.RawButton> { OVRInput.RawButton.B, OVRInput.RawButton.A, OVRInput.RawButton.B, OVRInput.RawButton.A, OVRInput.RawButton.RHandTrigger };
-    private List<KeyCode> buttonOrderEditorSeqList = new List<KeyCode> { KeyCode.T, KeyCode.Y, KeyCode.T, KeyCode.Y, KeyCode.Return };
+        private int currentPressedButtonIndex = -1;
 
-    // Settings ToggleButtonSeq
-    private List<OVRInput.RawButton> toggleButtonVRDevice = new List<OVRInput.RawButton> { OVRInput.RawButton.A,
+        // Game restart button Sequence
+        private List<OVRInput.RawButton> buttonOrderVRSeqList = new List<OVRInput.RawButton> { OVRInput.RawButton.B, OVRInput.RawButton.A, OVRInput.RawButton.B, OVRInput.RawButton.A, OVRInput.RawButton.RHandTrigger };
+        private List<KeyCode> buttonOrderEditorSeqList = new List<KeyCode> { KeyCode.T, KeyCode.Y, KeyCode.T, KeyCode.Y, KeyCode.Return };
+
+        // Settings ToggleButtonSeq
+        private List<OVRInput.RawButton> toggleButtonVRDevice = new List<OVRInput.RawButton> { OVRInput.RawButton.A,
                 OVRInput.RawButton.RHandTrigger};
-    private List<KeyCode> toggleButtonForEditor = new List<KeyCode> {    KeyCode.A,
-                KeyCode.S, }; 
+        private List<KeyCode> toggleButtonForEditor = new List<KeyCode> {    KeyCode.A,
+                KeyCode.S, };
 
 
-    private float waitingTimeForNextButtonPress;
-    private readonly float defaultWaitingTimeForNextButtonPress = 2f;
+        private float waitingTimeForNextButtonPress;
+        private readonly float defaultWaitingTimeForNextButtonPress = 2f;
 
 
-    [Header("LongPress")]
-    private bool longPress = true;
-    private float clickDuration = 1;
-    private bool thumpPressing = false;
-    private float totalThumpDownTime = 0;
-    private bool comboPressing = false;
-    private float totalComboDownTime = 0f;
+        [Header("LongPress")]
+        private bool longPress = true;
+        private float clickDuration = 1;
+        private bool thumpPressing = false;
+        private float totalThumpDownTime = 0;
+        private bool comboPressing = false;
+        private float totalComboDownTime = 0f;
 
 
 
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-        RestartGameInput();
-       if (SettingsManager.sharedInstance.canViewSettings)
-            LongPressThumbAndTrigger();
-
-    }
-
-    private bool IsVR()
-    {
-        try
+        // Start is called before the first frame update
+        void Start()
         {
-            return OVRManager.isHmdPresent;
-        }
-        catch
-        {
-            return false;
-        }
-    }
 
-    private void RestartGameInput()
-    {
-        if (!SettingsManager.sharedInstance.restartedCompleted) return;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+            RestartGameInput();
+            if (SettingsManager.sharedInstance.canViewSettings)
+                LongPressThumbAndTrigger();
+
+        }
+
+        private bool IsVR()
+        {
+            try
+            {
+                return OVRManager.isHmdPresent;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        private void RestartGameInput()
+        {
+            if (!SettingsManager.sharedInstance.restartedCompleted) return;
 
             // Split logic between VR (OVRInput) and Editor/standalone (KeyCode/Input)
             if (IsVR())
@@ -156,30 +156,30 @@ namespace Jambav.Settings
         }
 
 
-    public void LongPressThumpButton()
-    {
-        if (!longPress)
-            return;
+        public void LongPressThumpButton()
+        {
+            if (!longPress)
+                return;
 
-        #if UNITY_EDITOR_OSX || UNITY_STANDALONE_OSX
+#if UNITY_EDITOR_OSX || UNITY_STANDALONE_OSX
             HandleLongPressMac(KeyCode.Space, ref thumpPressing, ref totalThumpDownTime, clickDuration, () =>
             {
                 Debug.Log("thump long press (Mac test)");
             });
-        #else
+#else
             HandleLongPress(OVRInput.RawButton.RThumbstick, ref thumpPressing, ref totalThumpDownTime, clickDuration, () =>
             {
                 Debug.Log("thump long press");
             });
-        #endif
-    }
+#endif
+        }
 
-    public void LongPressThumbAndTrigger()
-    {
-        if (!longPress)
-            return;
+        public void LongPressThumbAndTrigger()
+        {
+            if (!longPress)
+                return;
 
-        #if UNITY_EDITOR_OSX || UNITY_STANDALONE_OSX
+#if UNITY_EDITOR_OSX || UNITY_STANDALONE_OSX
             HandleSimultaneousLongPressMac(
                 toggleButtonForEditor[0],
                toggleButtonForEditor[1],
@@ -187,7 +187,7 @@ namespace Jambav.Settings
                 ref totalComboDownTime,
                 clickDuration,
                 () => { OnSettingToggled?.Invoke(); });
-        #else
+#else
             HandleSimultaneousLongPress(
                 toggleButtonVRDevice[0],
                  toggleButtonVRDevice[1],
@@ -195,181 +195,181 @@ namespace Jambav.Settings
                 ref totalComboDownTime,
                 clickDuration,
                 () => { OnSettingToggled?.Invoke(); });
-        #endif
-    }
-
-    private bool HandleLongPress(OVRInput.RawButton button, ref bool isPressing, ref float totalDownTime, float duration, Action onLongPress)
-    {
-        if (OVRInput.GetDown(button))
-        {
-            totalDownTime = 0f;
-            isPressing = true;
+#endif
         }
 
-        if (isPressing && OVRInput.Get(button))
+        private bool HandleLongPress(OVRInput.RawButton button, ref bool isPressing, ref float totalDownTime, float duration, Action onLongPress)
         {
-            totalDownTime += Time.deltaTime;
-            if (totalDownTime >= duration)
+            if (OVRInput.GetDown(button))
+            {
+                totalDownTime = 0f;
+                isPressing = true;
+            }
+
+            if (isPressing && OVRInput.Get(button))
+            {
+                totalDownTime += Time.deltaTime;
+                if (totalDownTime >= duration)
+                {
+                    isPressing = false;
+                    onLongPress?.Invoke();
+                    return true;
+                }
+            }
+
+            if (isPressing && OVRInput.GetUp(button))
             {
                 isPressing = false;
-                onLongPress?.Invoke();
-                return true;
             }
+
+            return false;
         }
 
-        if (isPressing && OVRInput.GetUp(button))
+        private bool HandleLongPressMac(KeyCode key, ref bool isPressing, ref float totalDownTime, float duration, Action onLongPress)
         {
-            isPressing = false;
-        }
+            if (Input.GetKeyDown(key))
+            {
+                totalDownTime = 0f;
+                isPressing = true;
+            }
 
-        return false;
-    }
+            if (isPressing && Input.GetKey(key))
+            {
+                totalDownTime += Time.deltaTime;
+                if (totalDownTime >= duration)
+                {
+                    isPressing = false;
+                    onLongPress?.Invoke();
+                    return true;
+                }
+            }
 
-    private bool HandleLongPressMac(KeyCode key, ref bool isPressing, ref float totalDownTime, float duration, Action onLongPress)
-    {
-        if (Input.GetKeyDown(key))
-        {
-            totalDownTime = 0f;
-            isPressing = true;
-        }
-
-        if (isPressing && Input.GetKey(key))
-        {
-            totalDownTime += Time.deltaTime;
-            if (totalDownTime >= duration)
+            if (isPressing && Input.GetKeyUp(key))
             {
                 isPressing = false;
-                onLongPress?.Invoke();
-                return true;
             }
+
+            return false;
         }
 
-        if (isPressing && Input.GetKeyUp(key))
+        private bool HandleSimultaneousLongPress(OVRInput.RawButton buttonA, OVRInput.RawButton buttonB, ref bool isPressing, ref float totalDownTime, float duration, Action onLongPress)
         {
-            isPressing = false;
-        }
+            if (!isPressing)
+            {
+                if (OVRInput.GetDown(buttonA) || OVRInput.GetDown(buttonB))
+                {
+                    if (OVRInput.Get(buttonA) && OVRInput.Get(buttonB))
+                    {
+                        totalDownTime = 0f;
+                        isPressing = true;
+                    }
+                }
+                else if (OVRInput.Get(buttonA) && OVRInput.Get(buttonB))
+                {
+                    totalDownTime = 0f;
+                    isPressing = true;
+                }
+            }
 
-        return false;
-    }
-
-    private bool HandleSimultaneousLongPress(OVRInput.RawButton buttonA, OVRInput.RawButton buttonB, ref bool isPressing, ref float totalDownTime, float duration, Action onLongPress)
-    {
-        if (!isPressing)
-        {
-            if (OVRInput.GetDown(buttonA) || OVRInput.GetDown(buttonB))
+            if (isPressing)
             {
                 if (OVRInput.Get(buttonA) && OVRInput.Get(buttonB))
                 {
-                    totalDownTime = 0f;
-                    isPressing = true;
+                    totalDownTime += Time.deltaTime;
+                    if (totalDownTime >= duration)
+                    {
+                        isPressing = false;
+                        onLongPress?.Invoke();
+                        return true;
+                    }
                 }
-            }
-            else if (OVRInput.Get(buttonA) && OVRInput.Get(buttonB))
-            {
-                totalDownTime = 0f;
-                isPressing = true;
-            }
-        }
-
-        if (isPressing)
-        {
-            if (OVRInput.Get(buttonA) && OVRInput.Get(buttonB))
-            {
-                totalDownTime += Time.deltaTime;
-                if (totalDownTime >= duration)
+                else
                 {
                     isPressing = false;
-                    onLongPress?.Invoke();
-                    return true;
                 }
             }
-            else
+
+            if (OVRInput.GetUp(buttonA) || OVRInput.GetUp(buttonB))
             {
                 isPressing = false;
             }
+
+            return false;
         }
 
-        if (OVRInput.GetUp(buttonA) || OVRInput.GetUp(buttonB))
+        private bool HandleSimultaneousLongPressMac(KeyCode keyA, KeyCode keyB, ref bool isPressing, ref float totalDownTime, float duration, Action onLongPress)
         {
-            isPressing = false;
-        }
+            if (!isPressing)
+            {
+                if (Input.GetKeyDown(keyA) || Input.GetKeyDown(keyB))
+                {
+                    if (Input.GetKey(keyA) && Input.GetKey(keyB))
+                    {
+                        totalDownTime = 0f;
+                        isPressing = true;
+                        Debug.Log("Simultaneous press started");
+                    }
+                }
+                else if (Input.GetKey(keyA) && Input.GetKey(keyB))
+                {
+                    totalDownTime = 0f;
+                    isPressing = true;
+                    Debug.Log("Both keys held");
+                }
+            }
 
-        return false;
-    }
-
-    private bool HandleSimultaneousLongPressMac(KeyCode keyA, KeyCode keyB, ref bool isPressing, ref float totalDownTime, float duration, Action onLongPress)
-    {
-        if (!isPressing)
-        {
-            if (Input.GetKeyDown(keyA) || Input.GetKeyDown(keyB))
+            if (isPressing)
             {
                 if (Input.GetKey(keyA) && Input.GetKey(keyB))
                 {
-                    totalDownTime = 0f;
-                    isPressing = true;
-                    Debug.Log("Simultaneous press started");
+                    totalDownTime += Time.deltaTime;
+                    // Debug.Log($"Holding... {totalDownTime:F2}s / {duration}s");
+                    if (totalDownTime >= duration)
+                    {
+                        isPressing = false;
+                        onLongPress?.Invoke();
+                        Debug.Log("Long press");
+                        return true;
+                    }
                 }
-            }
-            else if (Input.GetKey(keyA) && Input.GetKey(keyB))
-            {
-                totalDownTime = 0f;
-                isPressing = true;
-                Debug.Log("Both keys held");
-            }
-        }
-
-        if (isPressing)
-        {
-            if (Input.GetKey(keyA) && Input.GetKey(keyB))
-            {
-                totalDownTime += Time.deltaTime;
-                // Debug.Log($"Holding... {totalDownTime:F2}s / {duration}s");
-                if (totalDownTime >= duration)
+                else
                 {
                     isPressing = false;
-                    onLongPress?.Invoke();
-                    Debug.Log("Long press");
-                    return true;
+                    Debug.Log("Key released - reset");
                 }
             }
-            else
+
+            if (Input.GetKeyUp(keyA) || Input.GetKeyUp(keyB))
             {
                 isPressing = false;
-                Debug.Log("Key released - reset");
+            }
+
+            return false;
+        }
+
+
+
+        private void ReloadScene()
+        {
+            Scene scene = SceneManager.GetActiveScene();
+            SceneManager.LoadScene(scene.name);
+        }
+
+
+        private IEnumerator RestartGame()
+        {
+            print("Restarting Game");
+            SettingsManager.sharedInstance.restartedCompleted = false;
+            SettingsManager.sharedInstance.OnRestartStart?.Invoke();
+            yield return new WaitForSeconds(1f);
+            ReloadScene();
+            yield return new WaitForSeconds(2);
+            SettingsManager.sharedInstance.restartedCompleted = true;
+
+            if (SettingsManager.sharedInstance.DoseGameReadyToPlay())
+            {
+                SettingsManager.sharedInstance.GameReadyToPlay?.Invoke();
             }
         }
-
-        if (Input.GetKeyUp(keyA) || Input.GetKeyUp(keyB))
-        {
-            isPressing = false;
-        }
-
-        return false;
     }
-
-
-
-    private void ReloadScene()
-    {
-        Scene scene = SceneManager.GetActiveScene();
-        SceneManager.LoadScene(scene.name);
-    }
-
-
-    private IEnumerator RestartGame()
-    {
-        print("Restarting Game");
-        SettingsManager.sharedInstance.restartedCompleted = false;
-        SettingsManager.sharedInstance.OnRestartStart?.Invoke();
-        yield return new WaitForSeconds(1f);
-        ReloadScene();
-        yield return new WaitForSeconds(2);
-        SettingsManager.sharedInstance.restartedCompleted = true;
-        
-        if(SettingsManager.sharedInstance.DoseGameReadyToPlay())
-        {
-            SettingsManager.sharedInstance.GameReadyToPlay?.Invoke();
-        }
-    }
-}
 }
